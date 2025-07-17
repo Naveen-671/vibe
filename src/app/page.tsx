fb7cc859147ddf547c926da57f9f0c355ae7759c
Naveen-671 <naveenr08012005@gmail.com>
2025-07-17 22:30:41 +0530

    03:trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
 // import Image from "next/image";
-import { prisma } from "@/lib/db";
-
+// import { prisma } from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>{JSON.stringify(users, null, 2)}</div>
-      <h1 className="text-4xl font-bold">Welcome to Vibe</h1>
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({
+      text: "world Prefetch"
+    })
+  );
 
-      <p className="text-lg text-center">
-        This is a simple app to demonstrate the use of Prisma with Next.js.
-      </p>
-    </div>
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
+// export default async function Home() {
+//   const users = await prisma.user.findMany();
+
+//   return (
+//     <div>
+//       <div>{JSON.stringify(users, null, 2)}</div>
+//       <h1 className="text-4xl font-bold">Welcome to Vibe</h1>
+
+//       <p className="text-lg text-center">
+//         This is a simple app to demonstrate the use of Prisma with Next.js.
+//       </p>
+//     </div>
+//   );
+// }
