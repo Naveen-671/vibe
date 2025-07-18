29367563b87dc8a64ad10954e64c1e7761238226
Naveen-671 <naveenr08012005@gmail.com>
2025-07-18 18:26:40 +0530

    06:e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-testz");
+      return sandbox.sandboxId;
+    });
+
     const summarizer = createAgent({
       name: "code-agent",
       system:
@@ -16,8 +23,12 @@ export const helloWorld = inngest.createFunction(
     const { output } = await summarizer.run(
       `Write the following snippets: ${event.data.value}`
     );
-    console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
