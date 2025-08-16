70f3ca866de353ac7032a2cfb3463938b1832446
Naveen-671 <naveenr08012005@gmail.com>
2025-08-16 20:35:52 +0530

    18.1: multiModels

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,4 @@
-// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   // eslint: {
-//   //   ignoreDuringBuilds: true
-//   // }
-// };
-
-// export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       // For general-purpose, high-quality stock photos
-//       { protocol: "https", hostname: "images.unsplash.com" },
-//       { protocol: "https", hostname: "images.pexels.com" },
-//       { protocol: "https", hostname: "cdn.pixabay.com" },
-
-//       // For business and e-commerce focused images
-//       { protocol: "https", hostname: "burst.shopify.com" },
-
-//       // For stock photos, vectors, and illustrations
-//       { protocol: "https", hostname: "img.freepik.com" },
-
-//       // For developer content, like avatars or images in repos
-//       { protocol: "https", hostname: "raw.githubusercontent.com" }
-//     ]
-//   }
-//   // eslint: {
-//   //   ignoreDuringBuilds: true
-//   // }
-// };
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
